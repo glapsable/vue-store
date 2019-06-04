5d65b510115a42666be11fa13474a09cbc9a366d
Glapson <glapsable@gmail.com>
2019-06-04 23:44:19 +0200

    add global and header styles

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,30 +1,43 @@
 <template>
   <header class="header">
-    <h1>TodoList</h1>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/contact">Contact</router-link>
-    </div>
+    <h1 class="header__logo">LOGO</h1>
+    <nav class="header__nav">
+      <router-link class="header__link" to="/">Home</router-link>
+      <router-link class="header__link" to="/about">About</router-link>
+      <router-link class="header__link" to="/contact">Contact</router-link>
+      <div><MagniferIcon /></div>
+    </nav>
   </header>
 </template>
 
 <script>
+  import MagniferIcon from '../../assets/icons/magnifier.svg';
   export default {
-    name: "Header"
+    name: "Header",
+    components: {
+      MagniferIcon
+    }
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  @import "../../assets/styles/_variables.scss";
   .header {
-    background: #333;
-    color: #fff;
-    text-align: center;
-    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid $gray;
+    height: 90px;
   }
-
-  .header a {
-    color: #fff;
-    padding-right: 5px;
+  .header__logo {
+    font-family: $second-font, sans-serif;
+    font-weight: $main-weight;
+    margin: 0;
+    padding: 0;
+  }
+  .header__link {
+    font-size: 14px;
+    font-weight: bold;
+    margin-right: 46px;
   }
 </style>
--- src/router.js
+++ src/router.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -14,18 +15,12 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import('./views/About.vue')
     },
     {
       path: '/contact',
       name: 'contact',
-      // route level code-splitting
-      // this generates a separate chunk (contact.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "contact" */ './views/Contact.vue')
+      component: () => import('./views/Contact.vue')
     }
   ]
 })
